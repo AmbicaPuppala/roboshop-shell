fafa87e9f146fb5b20681d8e12ccfa77db56e65e
ambica puppala <ambicapuppala9@gmail.com>
2026-06-25 15:16:22 +0530

    shell-script

---
+++ mysql.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+UserId=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+Log_Folder="/var/log/roboshop-logs"
+Log_File=$(echo $0 |cu -d "." -f1)
+Timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$Log_Folder/$Log_File-$Timestamp.log"
+
+VALIDATE(){
+    if [$1 ne 0]
+    then
+       echo -e "$2 ... $R FAILURE $N"
+    else
+       echo -e "$2 ...$G SUCCESS $N"
+    fi     
+}
+
+CHECK_ROOT(){
+    if [$UserId ne 0]
+then
+   echo "you must have root user access to execute scipt"
+   exit1
+fi
+}
+
+echo "script statrted executing at: $Timestamp" &>>LOG_FILE_NAME
+CHECK_ROOT
+
+dnf install mysql-server -y &>>LOG_FILE_NAME
+VALIDATE $? "Installing Mysql server"
+
+systemctl enable mysqld &>>LOG_FILE_NAME
+VALIDATE $? "Enabling Mysql server"
+
+systemctl start mysqld &>>LOG_FILE_NAME
+VALIDATE $? "starting Mysql server"
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>>LOG_FILE_NAME
+
+
+
+
