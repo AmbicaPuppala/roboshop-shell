4a1634023470d0e5afd76844403c9ec3886d1967
ambica puppala <ambicapuppala9@gmail.com>
2026-07-28 20:45:02 +0530

    script added

---
+++ shipping.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+Log_Folder="/var/log/roboshop-logs"
+Log_File=$(echo $0 |cut -d "." -f1)
+Timestamp=$(date +%Y-%m-%s-%H-%M-%S)
+LOG_FILE_NAME="$Log_Folder/$Log_File-$Timestamp.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$2...$R failure $N"
+    else
+        echo -e "$2...$G success $N"   
+    fi    
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "you must have root user access to execute scipt"
+        exit1
+    fi
+}
+
+echo "script started ececuting at : $Timestamp" &>>$LOG_FILE_NAME
+CHECK_ROOT
+
+dnf install maven -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Maven"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE_NAME
+VALIDATE $? "Adding user"
+
+mkdir /app &>>$LOG_FILE_NAME
+VALIDATE $? "creating directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE_NAME
+VALIDATE $? "downloading code"
+
+cd /app &>>$LOG_FILE_NAME
+VALIDATE $? "changing directory"
+
+unzip /tmp/shipping.zip
+VALIDATE $? "unzipping"
+
+mvn clean package 
+VALIDATE $? "clean package"
+
+mv target/shipping-1.0.jar shipping.jar 
+VALIDATE $? "moving jar file"
+
+cp /home/ec2-user/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "daemon reload"
+
+systemctl enable shipping  &>>$LOG_FILE_NAME
+VALIDATE $? "enabling shipping"
+
+systemctl start shipping &>>$LOG_FILE_NAME
+VALIDATE $? "starting shipping"
+
+dnf install mysql -y  &>>$LOG_FILE_NAME
+VALIDATE $? " installing mysql"
+
+mysql -h mysql.aslearnings.fun -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE_NAME
+VALIDATE $? "schema laoding"
+
+mysql -h mysql.aslearnings.fun -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE_NAME
+VALIDATE $? "app user authentication"
+
+mysql -h mysql.aslearnings.fun -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE_NAME
+VALIDATE $? "Load master data"
+
+systemctl restart shipping
+VALIDATE $? "restart shipping"
