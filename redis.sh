82ae7d2e333fe6fff5fd9ddd4ab241bead22db48
ambica puppala <ambicapuppala9@gmail.com>
2026-07-28 15:34:52 +0530

    script added

--- redis.sh
+++ redis.sh
@@ -40,9 +40,9 @@ VALIDATE $? "Enabling new version"
 dnf install redis -y &>>LOG_FILE_NAME
 VALIDATE $? "installing new version"
 
-sed -i 's/^[[:space:]]*bind[[:space:]].*/bind 0.0.0.0/' /etc/redis.conf
+sed -i 's/^[[:space:]]*bind[[:space:]].*/bind 0.0.0.0/' /etc/redis/redis.conf
 
-sed -i 's/^[[:space:]]*protected-mode[[:space:]]\+yes/protected-mode no/' /etc/redis.conf
+sed -i 's/^[[:space:]]*protected-mode[[:space:]]\+yes/protected-mode no/' /etc/redis/redis.conf
 
 systemctl enable redis &>>LOG_FILE_NAME
 VALIDATE $? "Enabling redis"
