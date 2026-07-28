005131e050cfbceece0db574c20868f5f6850e6d
ambica puppala <ambicapuppala9@gmail.com>
2026-07-28 11:21:58 +0530

    mongodb script

--- mongodb.sh
+++ mongodb.sh
@@ -5,8 +5,45 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+Log_Folder="/var/log/roboshop-logs"
+Log_File=$(echo $0 |cut -d "." -f1)
+Timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$Log_Folder/$Log_File-$Timestamp.log"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-       echo
+       echo -e "$2...$R failure $N"
+    else
+        echo -e "$2...$R failure $N"   
+    fi    
 }
+
+CHECK_ROOT(){
+    if[ $USERID -ne 0]
+    then
+       echo "you must have root user access to execute script"
+       exit1
+    fi
+}
+
+echo "script started executing"
+CHECK_ROOT
+
+
+cp /home/ec2-user/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org -y
+VALIDATE $? "installing mongodb"
+
+systemctl enable mongod
+VALIDATE $? "Enabling mongodb"
+
+systemctl start mongod
+VALIDATE $? "starting mongodb"
+
+sed -i 's/^#bindIp:.*/bindIp: 0.0.0.0/' /etc.mongod.conf
+
+systemctl restart mongod
+VALIDATE $? "restarts mongodb service" 
+
