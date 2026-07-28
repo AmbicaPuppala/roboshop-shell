7463c045a345aa77d161bbef901b70a071651b92
ambica puppala <ambicapuppala9@gmail.com>
2026-07-28 23:36:54 +0530

    script added

--- dispatch.sh
+++ dispatch.sh
@@ -58,11 +58,8 @@ VALIDATE $? "unzipping"
 go mod init dispatch &>>$LOG_FILE_NAME
 VALIDATE $? "dispatch"
 
-go get github.com/rabbitmq/amqp091-go &>>$LOG_FILE_NAME
-VALIDATE $? "get"
-
-go mod tidy &>>$LOG_FILE_NAME
-VALIDATE $? "get"
+go get &>>$LOG_FILE_NAME
+VALIDATE $? "build"
 
 go build &>>$LOG_FILE_NAME
 VALIDATE $? "build"
